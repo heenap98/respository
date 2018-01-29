523d6757cc1108f5fb728ad72ae6a38ca85ced90
Vesselin Grantcharov <ves@lawn-128-61-50-184.lawn.gatech.edu>
2018-01-29 11:46:49 -0500

    fix antlab thing

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -67,18 +67,14 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Arslan Munaf");
 		  toPrint += p3.toString("amunaf3");
-                  //Person4 replace P4 with your name
-                  //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("William Collins");
-                  toPrint += p4.toString("wcollins37");
-                  //Person5 replace P4 with your name
-                  //and gburdell5 with your gt id
-<<<<<<< HEAD
-                  Person5 p5 = new Person5("Ves");
-                  toPrint += p5.toString("Vdsu2");
-=======
-
->>>>>>> 20d54773b14322e3b705cbe69fd71eaa2f6f2244
+      //Person4 replace P4 with your name
+      //and gburdell4 with your gt id
+      Person4 p4 = new Person4("William Collins");
+      toPrint += p4.toString("wcollins37");
+      //Person5 replace P4 with your name
+      //and gburdell5 with your gt id
+      Person5 p5 = new Person5("Ves");
+      toPrint += p5.toString("vgrantcharov3");
 
         System.out.println(toPrint);
 
@@ -92,27 +88,6 @@ import edu.gatech.oad.antlab.pkg2.*;
      public static void main(String[] args) {
 
         new AntLabMain().printOutMessage();
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 20d54773b14322e3b705cbe69fd71eaa2f6f2244
-
      }
 
-
-
-
-<<<<<<< HEAD
- }
-=======
- }
-=======
-
-     }
-
-
-
-
- }
->>>>>>> Cameron
->>>>>>> 20d54773b14322e3b705cbe69fd71eaa2f6f2244
+   }
